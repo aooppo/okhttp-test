90c2eb8533a0d37e73a7b286caf09ec249a93a3a
TJ <i@voox.cc>
2022-10-27 09:10:12 +0800

    test

--- src/main/java/cc/voox/test/ConnTest.java
+++ src/main/java/cc/voox/test/ConnTest.java
@@ -9,29 +9,57 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.IntSupplier;
 import java.util.stream.Stream;
 
 public class ConnTest {
     final static OkHttpClient client = new OkHttpClient();
-
+    
     public static void main(String[] args) {
-        String s = "http://192.168.126.88/GetCurrencyList.aspx?routeDB=Montreal";
+        List<String> links = Arrays.asList("http://192.168.126.88/GetCurrencyList.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetAPPaymentTermList.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetVendorPaymentTermList.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetGLAccountList.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetExchangeRate.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetBatchNumber.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetARBatchId.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetTaxDetailList.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetGroups4CusOrVen.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetPaymentCodeList.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetCurrencyList.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetCustomerOutstandingTransactions.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetTaxDetailListForLines.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetGLAccountNameList.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetARBatchList.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetBankList.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetReceiptBatchList.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetAdjustmentBatchList.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetCustomerPastDueInfo.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetSchedPaymentList.aspx?routeDB=Montreal",
+                "http://192.168.126.88/GetReceiptBatchEntries.aspx?routeDB=Montreal");
+//        String s = "http://192.168.126.88/GetCurrencyList.aspx?routeDB=Montreal";
 //        for (int i = 0; i < 10000; i++) {
 //            System.out.println("execute index: " + i + " " + LocalDateTime.now().toString());
 //            testOkhttpSync(s, i);
 //            testOkhttpAsync(s, i);
 //        }
-        for (int i = 0; i < 2000; i++) {
-            final int j = i;
-            Runnable t2 = () -> {
-                for (int k = 0; k < 10000; k++) {
-                    testOkhttpSync(s + "&xx="+k, k);
-                }
-            };
-            new Thread( t2, "thread: " + j).start();
-        }
+        links.forEach(s -> {
+            for (int i = 0; i < 200; i++) {
+                final int j = i;
+                Runnable t2 = () -> {
+                    for (int k = 0; k < 10000; k++) {
+                        final int v = k;
+
+                        testOkhttpSync(s + "&xx="+v, v);
+                    }
+                };
+                new Thread( t2, "thread: " + j).start();
+            }
+        });
+      
 //        testOkhttpSync(s, 1);
 //        testOkhttpSync(s, 2);
         //hangs...
@@ -70,12 +98,12 @@ public class ConnTest {
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                System.out.println("index:" + i + " >> " + e.getMessage());
+                System.out.print("index:" + i + " >> " + e.getMessage());
             }
 
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                System.out.println("index: " + i + "  >> " + LocalDateTime.now().toString() + ">>> " + Objects.requireNonNull(response.body()).string());
+                System.out.print("index: " + i + "  >> " + LocalDateTime.now().toString() + ">>> " + Objects.requireNonNull(response.body()).string());
             }
         });
 
@@ -83,13 +111,14 @@ public class ConnTest {
 
     static void testOkhttpSync(String url, int i) {
 
-
+        System.out.println("request url: " + url);
         Request request = new Request.Builder()
                 .url(url)
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
-            System.out.print("Thread:"+ Thread.currentThread().getName()+"  index: " + i + "  >> " + LocalDateTime.now().toString() + ">>> " + response.body().string());
+            System.out.println("Thread:"+ Thread.currentThread().getName()+"  index: " + i + "  >> " + LocalDateTime.now().toString() + " response code >>> " + response.code());
+//            + response.body().string()
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
